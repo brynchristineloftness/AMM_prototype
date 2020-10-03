a0df387e85f8aa1fdcb52ad9d593def97a8f4518
Bryn Loftness <brynchristineloftness@yahoo.com>
2020-10-02 20:18:33 -0600

    12/13 /23

--- new_main.py
+++ new_main.py
@@ -735,9 +735,9 @@ for item in range(len(myfile['Type'])):
         autolist.append(myfile['TestName'][item])
 
 
-deletepack = []
+keep_pack = []
 
-
+#---------------
 round3 = (pack9)
 round3 = sortstuff(round3)
 round3real = []
@@ -745,8 +745,10 @@ for item in round3:
     if item[0] in autolist and item[1] in manuallist:
         round3real.append(item)
 round3 = round3real
-print('A',len(round3),1)
+
+print('three',len(round3),1)
 prototypecheck(round3)
+keep_pack += round3
 #-------
 round4 = pack15
 round4 = sortstuff(round4)
@@ -773,9 +775,13 @@ round4 = [x for x in round4 if x in prune_asserts]
 add_methods = setmetrics_combo(myfile,testlen,defaultgrid,"Methods",.9995)
 round4 = [x for x in round4 if x in add_methods]
 
-print('B',len(round4),2) 
-prototypecheck(round4)
 
+split = tfidf_model(myfile,testlen,.75,'bnn')
+round4 = [x for x in round4 if x not in split]
+
+print('four',len(round4),2) 
+prototypecheck(round4)
+keep_pack+=round4
 #-------------
 round1 = sortstuff(round1)
 round1 = [x for x in round1 if x not in prunepack]
@@ -783,9 +789,13 @@ round1 = [x for x in round1 if x not in prunepack]
 prune_asserts = setmetrics_combo(myfile,testlen,defaultgrid,"Methods_Asserts",.9999)
 round1 = [x for x in round1 if x in prune_asserts]
 
-print('C',len(round1),2) 
-prototypecheck(round1)
 
+split = tfidf_model(myfile,testlen,.785,'bnn')
+round1 = [x for x in round1 if x in split]
+
+print('one',len(round1),2) 
+prototypecheck(round1)
+keep_pack+=round1
 #----------------------------
 
 round6 = sortstuff(round6)
@@ -797,17 +807,16 @@ round6 = [x for x in round6 if x not in add_methods]
 prune_scenario = setmetrics_combo(myfile,testlen,defaultgrid,"Scenario",.7)
 round6 = [x for x in round6 if x not in prune_scenario]
 
+split = tfidf_model(myfile,testlen,.605,'bnn')
+round6 = [x for x in round6 if x not in split]
+
 round6 = [x for x in round6 if x not in prunepack]
 
-print('D',len(round6),2)
+print('six',len(round6),2)
 prototypecheck(round6)
-
+keep_pack+=round6
 #-----------
 round7 = sortstuff(round7)
-
-add_methods = setmetrics_combo(myfile,testlen,defaultgrid,"Methods_Asserts",.65)
-round8 = [x for x in round7 if x in add_methods]
-round7 = [x for x in round7 if x not in add_methods]
 
 prune_scenario = setmetrics_combo(myfile,testlen,defaultgrid,"Scenario",.69)
 round7 = [x for x in round7 if x not in prune_scenario]
@@ -815,67 +824,47 @@ round7 = [x for x in round7 if x not in prune_scenario]
 prune_scenario = setmetrics_combo(myfile,testlen,defaultgrid,"Combo",.59)
 round7 = [x for x in round7 if x not in prune_scenario]
 
+
+split = tfidf_model(myfile,testlen,.58,'bnn')
+round7 = [x for x in round7 if x not in split]
+
 round7 = [x for x in round7 if x not in prunepack]
 
-
-
-print('E',len(round7),1)
+print('seven',len(round7),3)
 prototypecheck(round7)
-
+keep_pack+=round7
 #----------------------------
-
-round8 = sortstuff(round8)
+add_methods = setmetrics_combo(myfile,testlen,defaultgrid,"Methods_Asserts",.65)
+round8 = [x for x in round7 if x in add_methods]
 
 prune_scenario = setmetrics_combo(myfile,testlen,defaultgrid,"Combo",.4)
 round8 = [x for x in round8 if x in prune_scenario]
 
-split = tfidf_model(myfile,testlen,.6,'bnn')
-round9 = [x for x in round8 if x not in split]
-round8 = [x for x in round8 if x in split]
+
+split = tfidf_model(myfile,testlen,.58,'bnn')
+round8 = [x for x in round8 if x not in split]
+
 
 round8 = [x for x in round8 if x not in prunepack]
-
-print('F',len(round8),2)
+print('eight',len(round8),2)
 prototypecheck(round8)
-
-
-#----------------------------
-round9 = sortstuff(round9)
-
-split = tfidf_model(myfile,testlen,.3,'bfc')
-round9 = [x for x in round9 if x not in split]
-
-round9 = [x for x in round9 if x not in prunepack]
-print('G',len(round9),2)
-prototypecheck(round9)
-
+keep_pack+=round8
 #+++++++++++++++++++++++++
 
-
-epic1 = round3+round4+round1+round6+round7+round8+round9
-
-
-epic1 = sortstuff(epic1)
+epic1 = keep_pack
 
 print(len(epic1))
 counter = 0
 count = 0
 counter2 = 0
-question1 = sorted(epic1)[0][0]
-questionnum =1
 for item in sorted(epic1):
-    if question1 != item[0]:
-        question1 = item[0]
-        questionnum+=1
-        print()
-        count = 0
     if item in oracle :
-        print('*o*',end='')
+        print('*o*',item,end='')
         counter+=1
     if item in mpmoracle:
         print("*mpm*", end = '')
         counter2+=1
-    print (item,end = '')
+    #print (item,end = '')
     if item in round1:
         print('r1')
     elif item in round3:
@@ -886,8 +875,6 @@ for item in sorted(epic1):
         print('r6')
     elif item in round7 :
         print('r7')
-    elif item in round8 :
-        print('r8')
     elif item in round9:
         print('r9')
 
